d2a3958850c6f23cee43585f544aebcb1c6ec503
KellyPT <trankphuong@gmail.com>
2020-10-24 19:10:26 -0700

    install and configure Style and CSS Loaders

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -12,6 +12,10 @@ const config = {
         use: "babel-loader",
         test: /\.js$/,
       },
+      {
+        use: ["style-loader", "css-loader"], // loaders will be applied from right to left
+        test: /\.css$/,
+      },
     ],
   },
 };
